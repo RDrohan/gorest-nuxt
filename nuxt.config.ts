59b60f27a014e5fcb42ca4edf54a70eacbd186d4
Ricky Drohan <rdrohanirl@yahoo.ie>
2022-12-28 15:44:21 +0000

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    apiKey: process.env.GOREST_API_KEY,
+  },
+});
