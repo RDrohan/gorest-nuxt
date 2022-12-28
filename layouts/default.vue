614a9f759567901400b570384d9f6c3c8c76bb84
Ricky Drohan <rdrohanirl@yahoo.ie>
2022-12-28 16:06:07 +0000

    Created holding page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,16 @@
 <template>
-  <div class="alert shadow-lg">
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="stroke-info flex-shrink-0 w-6 h-6"
+  <div class="container mx-auto sticky top-0 z-50 navbar">
+    <div class="flex-1">
+      <NuxtLink to="/" class="fbtn btn-ghost normal-case text-xl"
+        >GO REST Nuxt</NuxtLink
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        ></path>
-      </svg>
-      <span>12 unread messages. Tap to see.</span>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+      </ul>
     </div>
   </div>
+  <slot />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Welcome to the GOREST Nuxt app!</h1>
+        <p class="py-6">More to come ...</p>
+      </div>
+    </div>
   </div>
 </template>
